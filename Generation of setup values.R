d25931f2e0bcfac07fb0a244306b34416e194bca
Nick Lauerman <nicholas.lauerman@gmail.com>
2019-11-04 14:44:25 -0600

    alg finalized

--- Generation of setup values.R
+++ Generation of setup values.R
@@ -1,11 +1,7 @@
-roomNames <- NULL
-halls <- NULL
-roomNames <- c("Surf1",
-               "Turf1",
-               "Surf2",
-               "Turf2",
-               "Fin",
-               "Feather",
-               "Hook",
-               "Crook")
-Halls <- 1:7
+halls <- 1:8
+doorSymbols <- c("Surf",
+                 "Turf",
+                 "Fin",
+                 "Feather",
+                 "Hook",
+                 "Crook")
--- Master.R
+++ Master.R
@@ -1,2 +1,29 @@
 library(tidyverse)
 source(file = "./Generation of setup values.R")
+makeRoom <- function(){
+     doors <- sample(doorSymbols, 6)
+     outHall <- sample(halls,1)
+     fountainPresent <- sample(c(TRUE,FALSE),1)
+     if(fountainPresent){
+          fountainPosion <- sample(c("Yes","No"),1)
+     } else {
+          fountainPosion <- " "
+     }
+     hold <- NULL
+     hold <- data.frame(roomName = " ",
+                        outHallway = outHall,
+                        FountainPresent = fountainPresent,
+                        FountainPosioned = fountainPosion,
+                        door1 = doors[1],
+                        door2 = doors[2],
+                        door3 = doors[3],
+                        door4 = doors[4],
+                        door5 = doors[5],
+                        door6 = doors[6])
+     return(hold)
+     }
+roomList <- NULL
+for(i in 1:1000){
+     currentRoom <- makeRoom()
+     roomList <- rbind(roomList,currentRoom)
+}

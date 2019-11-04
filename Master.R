bfd0fb03dc404db52a1d23046a5db58b59c0f5eb
Nick Lauerman <nicholas.lauerman@gmail.com>
2019-11-04 15:20:31 -0600

    stop

--- Master.R
+++ Master.R
@@ -23,7 +23,39 @@ makeRoom <- function(){
      return(hold)
      }
 roomList <- NULL
-for(i in 1:1000){
+for(i in 1:32){
      currentRoom <- makeRoom()
      roomList <- rbind(roomList,currentRoom)
 }
+roomList$FountainPresent[1] <- "Yes"
+roomList$FountainPosioned[1] <- " "
+startState <- NULL
+RoomName <- c("Initial",
+              rep_len(" ", 7))
+OutHallway <- c(1,2,3,4,5,6,7,8)
+InitHalls <- NULL
+fountainPresent <- c("No",
+                     rep_len(c("No","Yes"), 7))
+fountainPosion <- c(" ",
+                    rep_len(c(" ","No"), 7))
+
+for(i in 1:8){
+     doors <- sample(doorSymbols, 6)
+     hold <- NULL
+     hold <- data.frame(roomName = RoomName[i],
+                        outHallway = OutHallway[i],
+                        FountainPresent = fountainPresent[i],
+                        FountainPosioned = fountainPosion[i],
+                        door1 = doors[1],
+                        door2 = doors[2],
+                        door3 = doors[3],
+                        door4 = doors[4],
+                        door5 = doors[5],
+                        door6 = doors[6])
+     InitHalls <- rbind(InitHalls, hold)
+}
+
+hallList <- rbind(InitHalls, roomList)
+str(InitHalls)
+
+
